135e31def67c8d31eeb8497b42a1a636467e6f12
kheneahm <kheneahm9@gmail.com>
2021-03-19 16:48:24 -0500

    Accept Invitation Endpoint

---
+++ TravelogApi/Controllers/InviteController.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace TravelogApi.Controllers
+{
+    public class InviteController : Controller
+    {
+        private readonly IPlanInvitationRepository _planInvitationRepository;
+
+        public InviteController(IPlanInvitationRepository planInvitationRepository)
+        {
+            _planInvitationRepository = planInvitationRepository;
+        }
+
+        public async Task<IActionResult> Accept(int inviteId)
+        {
+            try
+            {
+                var loggedInUserId = HttpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value;
+
+                await _planInvitationRepository.AcceptInvitation(new Guid(loggedInUserId), inviteId);
+
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
